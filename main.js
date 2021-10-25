37f57105c5341b2d455be3740c59c87ad0feb3a5
Ali Pandidan <ali.pandidan@swedbank.se>
2021-10-25 21:02:06 +0200

    Implement notifyPullRequest

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const moment = require('moment')
 const axios = require('axios').default
+require('colors');
 require('dotenv').config()
 
 axios.interceptors.request.use(function (config) {
@@ -18,11 +19,12 @@ async function main() {
             let _merge = await merge(pullRequest.id) // Check for merge vetoes or do merge if all checks are passed
 
             if (isSuccessfull(_merge)) {
-                log("Pull request #" + pullRequest.id + " successfully merged")
-            } else if (isFailed(_merge)) { // Check if merge is rejected due to an out of date branch
+                log(("Pull request #" + pullRequest.id + " successfully merged (No rebase needed)").green)
+            } else if (isFailed(_merge)) {
 
                 log("Failed merging pull request #" + pullRequest.id + ": " + JSON.stringify(_merge.data.errors))
 
+                // Check if merge is rejected due to an out of date branch
                 if (hasOne(_merge.data.errors) && _merge.data.errors.some(error => error.message.includes('configured to require fast-forward merges'))) {
                     let _rebase = await rebase(pullRequest.id) // Do rebase
 
@@ -40,7 +42,8 @@ async function main() {
                             await wait(10)
 
                             // Should keep loop alive if there's only one error of type "com.atlassian.bitbucket.pull.PullRequestMergeVetoedException" where "detailedMessage" says either  "it has in-progress builds" or "need a minimum of one successful build"
-                            //
+                            // [{"context":null,"message":"Merging the pull request has been vetoed.","exceptionName":"com.atlassian.bitbucket.pull.PullRequestMergeVetoedException","conflicted":false,"vetoes":[{"summaryMessage":"Not all required builds are successful yet","detailedMessage":"You still need a minimum of one successful build before this pull request can be merged."},
+                            // {"summaryMessage":"Requires approvals","detailedMessage":"You still need 2 approvals before this pull request can be merged."},{"summaryMessage":"Not all required reviewers have approved yet","detailedMessage":"At least 2 of the following users must review and approve this pull request before it can be merged."}]}]
                             // [{"context":null,"message":"Merging the pull request has been vetoed.","exceptionName":"com.atlassian.bitbucket.pull.PullRequestMergeVetoedException","conflicted":false,"vetoes":[{"summaryMessage":"Not all required builds are successful yet","detailedMessage":"You still need a minimum of one successful build before this pull request can be merged."}]}]
                             // [{"context":null,"message":"Merging the pull request has been vetoed.","exceptionName":"com.atlassian.bitbucket.pull.PullRequestMergeVetoedException","conflicted":false,"vetoes":[{"summaryMessage":"Not all required builds are successful yet","detailedMessage":"You cannot merge this pull request while it has in-progress builds."}]}]
 
@@ -63,13 +66,14 @@ async function main() {
             }
 
         } else {
-            log('Pull request #' + pullRequest.id + ' is not labeled as automerge')
+            log(('Pull request #' + pullRequest.id + ' is not labeled as automerge').yellow)
         }
 
         await wait(2)
 
     }
 
+    log("Finished parsing all pull requests, restarting in 5 seconds\n".cyan)
     setTimeout(main, 5 * 1000);
 }
 
@@ -90,11 +94,11 @@ function isFailed(axios) {
 }
 
 function log(message) {
-    console.log('[' + moment().format('YYYY/MM/DD HH:mm:ss') + '] ' + message)
+    console.log(('[' + moment().format('YYYY/MM/DD HH:mm:ss') + '] ').gray + message)
 }
 
 async function rebase(pullRequestId) {
-    log("Rebasing pull request #" + pullRequestId)
+    log("[" + arguments.callee.name + "] + Rebasing pull request #" + pullRequestId)
     try {
         let latestPullRequestRevision = await getPullRequest(pullRequestId)
         return await axios.post(process.env.BITBUCKET_PR_GIT_API + pullRequestId + '/rebase', {
@@ -119,18 +123,32 @@ async function merge(pullRequestId) {
     }
 }
 
+async function notifyPullRequest(pullRequestId) {
+    try {
+        return await axios.post(process.env.BITBUCKET_PR_API + pullRequestId + '/comments', {
+            text: ":robot: Automerge bot is running",
+        })
+
+    } catch (error) {
+        return error.response
+    }
+}
+
 async function isAutoMerge(pullRequestId) {
-    log("Checking if pull request #" + pullRequestId + " has auto merge label")
+    log(("Checking if pull request #" + pullRequestId + " has auto merge label").yellow)
     try {
         let activities = await axios.get(process.env.BITBUCKET_PR_API + pullRequestId + '/activities')
-        for (activity of activities.data.values) {
-            if (activity.action == 'COMMENTED' &&
-                activity.comment.text == '>automerge' &&
-                (!process.env.ALLOWED_USERS || process.env.ALLOWED_USERS.split(',').includes(activity.user.name))
-            ) {
-                return true
-            }
-        }
+
+        let comments = activities.data.values.filter(activity => activity.action === 'COMMENTED')
+
+        // if(!comments.find(activity => activity.comment.text.includes("Automerge bot is running"))) {
+        //     notifyPullRequest(pullRequestId)
+        // }
+
+        let isAutoMergeAllowed = comments.find(activity => activity.comment.text === '>automerge' && (!process.env.ALLOWED_USERS || process.env.ALLOWED_USERS.split(',').includes(activity.user.name)))
+
+        return Boolean(isAutoMergeAllowed)
+
     } catch(error) {
         console.log(error.response.data)
         return false
@@ -138,7 +156,7 @@ async function isAutoMerge(pullRequestId) {
 }
 
 async function getPullRequests() {
-    log("Getting list of pull requests")
+    log("Getting list of pull requests".cyan)
     try {
         let pullRequests =  await axios.get(process.env.BITBUCKET_PR_API)
         return pullRequests.data.values
@@ -149,7 +167,7 @@ async function getPullRequests() {
 }
 
 async function getPullRequest(pullRequestId) {
-    log("Getting latest pull request revision #" + pullRequestId)
+    log(("Getting latest pull request revision #" + pullRequestId).cyan)
     try {
         let pullRequest =  await axios.get(process.env.BITBUCKET_PR_API + pullRequestId)
         return pullRequest.data
